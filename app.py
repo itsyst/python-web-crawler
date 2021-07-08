bd406127493b6b83676553215743b1dd5617a0b0
Khaled El Hamzi <contact@elhamzi.me>
2021-07-09 00:27:46 +0200

    get questions

--- app.py
+++ app.py
@@ -5,7 +5,14 @@ import config
 response = requests.get(config.url)
 soup = BeautifulSoup(response.text, "html.parser")
 
+
 questions = soup.select(".question-summary")
+# print(type(questions[0]))
+# print(questions[0].attrs)
+# print(questions[0].["id"])
+# print(questions[0].get("id", 0))
+# print(questions[0].select(".question-hyperlink"))
+
 for question in questions:
     print(question.select_one(".question-hyperlink").getText())
-    print(question.select_one(".vote-count-post").getText())
+    

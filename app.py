5a028c439d185d473a257f56aa1f913003ae44f4
Khaled El Hamzi <contact@elhamzi.me>
2021-07-09 00:28:00 +0200

    get votes

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ questions = soup.select(".question-summary")
 
 for question in questions:
     print(question.select_one(".question-hyperlink").getText())
-    
+    print(question.select_one(".vote-count-post").getText())
